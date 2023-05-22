3a5149af9bec3e6345271e08fb14da349472293b
Santanu <santanuworkspace@gmail.com>
2023-05-22 11:23:07 +0530

    improved the charcter value using charAt()

--- CommandLine.java
+++ CommandLine.java
@@ -4,7 +4,7 @@ public class CommandLine{
 		// args[] stores the input entered at command line 
 		// it is a array string 
 		System.out.println("String: "+args[0]);
-		System.out.println("char: "+args[1]); 
+		System.out.println("char: "+args[1].charAt(0)); 
 		System.out.println("Int: "+Integer.parseInt(args[2])); // parsing to int
 		System.out.print("Float: "+Float.parseFloat(args[3])); // parsing to float
 	}
