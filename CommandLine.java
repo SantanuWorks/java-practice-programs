ba327a912923c5d12e2f41cbff13e6ebec94c3f3
Santanu <santanuworkspace@gmail.com>
2023-05-22 11:11:14 +0530

    Command-line argument program

---
+++ CommandLine.java
@@ -0,0 +1,11 @@
+// command-line argument 
+public class CommandLine{
+	public static void main(String args[]){
+		// args[] stores the input entered at command line 
+		// it is a array string 
+		System.out.println("String: "+args[0]);
+		System.out.println("char: "+args[1]); 
+		System.out.println("Int: "+Integer.parseInt(args[2])); // parsing to int
+		System.out.print("Float: "+Float.parseFloat(args[3])); // parsing to float
+	}
+}
