a30970e07c716826e45fb1d66835fbcf605cba6f
Santanu <santanuworkspace@gmail.com>
2023-05-25 23:32:52 +0530

    Triangle validation on the basis of side

---
+++ practice-problems/ValidTriangleSide.java
@@ -0,0 +1,12 @@
+// program to check whether a triangle is valid or not 
+// if sum of two side is greater than the third then triangle is valid
+// input through command line
+public class Leap{
+	public static void main(String[] args){
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		if( (a+b)>c && (b+c)>a && (a+c)>b ) System.out.print("It is a valid trianlge");
+		else System.out.print("It is not a valid trianlge");
+	}
+}
