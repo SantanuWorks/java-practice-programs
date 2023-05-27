50d26bb1518cfe9e508c763f89a0c8979566b022
Santanu <santanuworkspace@gmail.com>
2023-05-27 11:08:40 +0530

    Factorial program

---
+++ practice-problems/Factorial.java
@@ -0,0 +1,10 @@
+// find the factorial of a given number 
+// input the number through the command line
+public class Factorial{
+	public static void main(String[] args){
+		int num = Integer.parseInt(args[0]);
+		long fact = 1 ;
+		for( int i = num ; i > 1 ; i-- ) fact *= i ;
+		System.out.print("Factorial of "+num+" is "+fact);
+	}
+}
