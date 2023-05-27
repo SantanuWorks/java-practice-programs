0807ed046ef227a6b9c930568507c8a135a9dcfc
Santanu <santanuworkspace@gmail.com>
2023-05-27 11:32:55 +0530

    improvement

--- practice-problems/Ascii.java
+++ practice-problems/Ascii.java
@@ -1,8 +1,6 @@
 // print all the ASCII values and their equivalent characters
 public class Ascii{
 	public static void main(String[] args){
-		for( char i = 0 ; i < 256 ; i ++ ){
-			System.out.println((int)i+" -> "+i);
-		}
+		for( char i = 0 ; i < 256 ; i ++ ) System.out.println((int)i+" -> "+i);
 	}
 }
