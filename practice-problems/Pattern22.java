630c155d25b18b804e24abed2479a88a0b1ed068
Santanu <santanuworkspace@gmail.com>
2023-05-30 12:22:48 +0530

    Pattern programming 22

---
+++ practice-problems/Pattern22.java
@@ -0,0 +1,30 @@
+// program to print following pattern
+//
+// *     *
+// **   **
+// *** ***
+// *******
+// *** ***
+// **   **
+// *     *
+// 
+public class Pattern22{
+	public static void main(String[] args){
+		int step = 15 ;
+		for( int j = 0 ; j < step - 1 ; j ++ ){
+			for( int i = 0 ; i <= j ; i ++ ) System.out.print("*");
+			for( int i = 1 ; i < (step - 1 - j)*2  ; i ++ ) System.out.print(" ");
+			for( int i = 0 ; i <= j ; i ++ ) System.out.print("*");
+			System.out.println();
+		}
+		for( int i = 0 ; i < step*2 - 1; i ++ ) System.out.print("*");
+		System.out.println();
+		step-- ;
+		for( int j = 0 ; j < step ; j ++ ){
+			for( int i = 0 ; i < step - j ; i ++ ) System.out.print("*");
+			for( int i = 0 ; i < j*2 + 1 ; i ++ ) System.out.print(" ");
+			for( int i = 0 ; i < step - j ; i ++ ) System.out.print("*");
+			System.out.println();
+		}
+	}
+}
