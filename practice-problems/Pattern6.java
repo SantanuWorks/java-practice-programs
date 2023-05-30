1bc6897ee2dcfaa5492d95c8ed2111095558ccd8
Santanu <santanuworkspace@gmail.com>
2023-05-30 12:22:36 +0530

    Pattern programming 6

---
+++ practice-problems/Pattern6.java
@@ -0,0 +1,20 @@
+// program to print following pattern
+//
+//      *
+//     **        
+//    ***       
+//   ****       
+//  *****                
+// 
+public class Pattern6{
+	public static void main(String[] args){
+		int step = 5 ;
+		for( int i = 1 ; i <= step ; i++ ){
+			for( int j = 0 ; j < step - i ; j ++ )
+				System.out.print(" ");
+			for( int j = 0 ; j < i ; j ++ )
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
