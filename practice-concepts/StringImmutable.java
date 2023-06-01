b19734c46a8ac688164e5758d674fb2357230557
Santanu <santanuworkspace@gmail.com>
2023-06-01 13:19:21 +0530

    Program to demonstrate the property of immutability

---
+++ practice-concepts/StringImmutable.java
@@ -0,0 +1,15 @@
+// program to demonstrate various methods of string creation
+public class StringCreate{
+	public static void main(String[] args){
+		// string declared
+		String str = "Computer";
+		System.out.println(str);
+		// trying to change the str
+		str.concat(" Networking"); // method is used to add a string to the next of existing string
+		// instead of changing...it will create a new object with value "Computer Networking"
+		System.out.println(str.concat(" Networking")); // since str does not have reference to "Computer Networking"
+		// force the reference
+	    str = str.concat(" Networking");
+		System.out.print(str);
+	}
+}
