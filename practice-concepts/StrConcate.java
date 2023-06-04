9cd86983442f6d4e7fc30cbaaf5c529a9c978adc
Santanu <santanuworkspace@gmail.com>
2023-06-04 12:00:59 +0530

    Concatanation of string with other types

---
+++ practice-concepts/StrConcate.java
@@ -0,0 +1,19 @@
+// Program to demonstrate concatanation of String 
+public class StrConcate{
+	public static void main(String[] args){
+		// Add char, int, float to string to string
+		char ch = 'A' ;
+		int in = 10 ;
+		float fl = 12.3f ;
+		String str = "It is ";
+		String con = str + ch ;
+		System.out.println(con);
+		con = str + in ;
+		System.out.println(con);
+		con = str + fl ;
+		System.out.println(con);
+		// convert char to string
+		str = ""+ch ;
+		System.out.print(str);
+	}
+}
