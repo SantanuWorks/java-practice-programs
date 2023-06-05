177f625f35d2d001177d020f62a8dd26d11b17d6
Santanu <santanuworkspace@gmail.com>
2023-06-05 12:08:41 +0530

    Improved the input method

--- practice-problems/StrRemWord.java
+++ practice-problems/StrRemWord.java
@@ -1,14 +1,17 @@
 // Write a Java program that removes a specified word from given text.
 // Return the updated string
-// Input through command line
+import java.util.Scanner ;
 public class StrRemWord{
 	public static void main(String[] args){
-		StringBuffer text = new StringBuffer(args[0]);
-		System.out.println(text);
-		String str = args[1];
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the text: ");
+		StringBuffer text = new StringBuffer(sc.nextLine());
+		System.out.print("Enter the string to be deleted: ");
+		String str = sc.nextLine();
 		int start = text.indexOf(str) ;
 		int last = start + str.length() - 1 ;
 		String remtext = text.delete(start,last).toString();
 		System.out.print(remtext);
+		sc.close();
 	}
 }
