aa5e2326e6c2a7294c7bab9aecea18c6a00414d3
Santanu <santanuworkspace@gmail.com>
2023-06-06 16:05:44 +0530

    Program to demonstrate use of array 6

---
+++ practice-concepts/Array6.java
@@ -0,0 +1,19 @@
+// Program to demonstrate use of array
+public class Array5{
+	public static void main(String[] args){
+		// array index should lie between 0 and len-1
+		// otherwise there will be an exception called as
+		// ArrayIndexOutOfBoundsException
+		int[] arr = new int[2];
+		
+		// array can be initialized using index
+		arr[0] = 12 ; // okay
+		arr[1] = 45 ; // okay 
+		// index equals to length of array
+		arr[2] = 23 ; // #ArrayIndexOutOfBoundsException
+		// index is negative
+		arr[-1] = 23 ; // #ArrayIndexOutOfBoundsException
+		// index greater than length of array
+		arr[5] = 23 ; // #ArrayIndexOutOfBoundsException
+	}
+}
