be60e4bf981285a7085299a8b2fb6987d378e20e
Santanu <santanuworkspace@gmail.com>
2023-06-06 16:05:42 +0530

    Program to demonstrate use of array 2

---
+++ practice-concepts/Array2.java
@@ -0,0 +1,14 @@
+// Program to demonstrate use of array
+public class Array2{
+	public static void main(String[] args){
+		// initializing array with values
+		// array literals - when values are known early
+		int arr[] = new int[]{1,2,3};
+		// we can skip new int[]
+		byte arr1[] = { 2,4,5 };
+		char arr2[] = { 'a','b','c' };
+		short arr3[] = { 232,234,132 };
+		long[] arr4 = { 12L, 231, 233 };
+		String[] arr5 = { "ram","shyam","gita" };
+	}
+}
