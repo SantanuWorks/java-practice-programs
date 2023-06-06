1b67d961fa7c104ec41ca1194e5d865c1ced1445
Santanu <santanuworkspace@gmail.com>
2023-06-06 16:05:41 +0530

    Program to demonstrate use of array 1

---
+++ practice-concepts/Array1.java
@@ -0,0 +1,21 @@
+// Program to demonstrate use of array
+public class Array1{
+	public static void main(String[] args){
+		// declaring an array
+		int arr1[];
+		char arr2[];
+		float arr3[];
+		boolean arr4[];
+		
+		// Instantiating array
+		arr1 = new int[5] ;
+		arr2 = new char[5] ;
+		arr3 = new float[5] ;
+		arr4 = new boolean[5] ;
+		
+		// combining two steps 
+		short[] narr1 = new short[5];
+		byte narr2[] = new byte[5];
+		long []narr3 = new long[5];
+	}
+}
