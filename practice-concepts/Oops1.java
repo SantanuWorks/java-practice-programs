9c399647c980eaef55ff9f9131dff9a51ce7f11a
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-17 21:22:45 +0530

    Program to demonstrate OOP concept 1

---
+++ practice-concepts/Oops1.java
@@ -0,0 +1,12 @@
+// Program to demonstrate the class 
+public class Oops1{
+	public static void main(String[] args){
+		System.out.print("class declaration in java");
+	}
+}
+// it is a class
+class MyClass{
+	// declare variables here - called property
+	int classno = 1 ;
+	String classname = "MyClass" ;
+}
