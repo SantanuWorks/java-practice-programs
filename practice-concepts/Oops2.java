77da7d43c2866233913f333143ade0368ca18a63
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-17 21:22:47 +0530

    Program to demonstrate OOP concept 2

---
+++ practice-concepts/Oops2.java
@@ -0,0 +1,19 @@
+// Program to demonstrate the class 
+// now lets create an object 
+public class Oops1{
+	public static void main(String[] args){
+		System.out.println("class declaration in java");
+		// object creation
+		MyClass mcobj ; // declared 
+		mcobj = new MyClass(); // instantiated - memory allocated
+		// access the property
+		System.out.println("Accessing property 'classno' => "+mcobj.classno);
+		System.out.print("Accessing property 'classname' => "+mcobj.classname);
+	}
+}
+// it is a class
+class MyClass{
+	// declare variables here - called property
+	int classno = 1 ;
+	String classname = "MyClass" ;
+}
