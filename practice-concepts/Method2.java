9fb10fb83dd26ece0b78c63d14eee95429e3bc14
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-19 16:36:51 +0530

    Program to demonstrate method in java 2

---
+++ practice-concepts/Method2.java
@@ -0,0 +1,26 @@
+// Program to demonstrate the Methods in java
+public class Method2{
+	static String name = "JAVA";
+	static int version = 17;
+	// user defined method defination
+	public String getName(){
+		// instance methods can access static variables directly
+		return name;
+	}
+	public static void display(){
+		// static methods cann't access instance method directly
+		Method2 m = new Method2();
+		System.out.println("Hey, I'm "+m.getName());
+		// static methods can access static variables directly
+		System.out.print("Version "+version+".0.1");
+	}
+	
+	public static void main(String[] args){
+		// static methods are part of class - shared with objects
+		Method2 m = new Method2();
+		// class-name, object can be used to call static methods
+		Method2.display();
+		// we can omit if caller method is aslo static
+		// display();
+	}
+}
