f4acd0c204145cd406acab8e17a04e2487f2b11d
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-21 21:59:29 +0530

    Program to demonstrate abstraction in java 1

---
+++ practice-concepts/Abstraction1.java
@@ -0,0 +1,21 @@
+// Program to demonstrate the concept of abstraction
+public abstract class Test{
+	int a;
+	// contructors
+	Test(){ System.out.println("Abstraction"); }
+	Test(int a){ this.a = a; }
+	// static method
+	static void display(){ System.out.println("Abstraction"); }
+	// abstract method
+	abstract void calc();
+	// final method
+	final void reside(){ System.out.println("Abstraction"); }
+	// concrete method
+	int drop(){ return 1; }
+}
+public class Abstraction1{
+	public static void main(String[] args){
+		// cann't create object of Test - this will show error
+		Test t = new Test();
+	}
+}
