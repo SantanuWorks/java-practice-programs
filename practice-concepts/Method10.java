fbdfc0c9c9da3da967a3b0fc28da7e10544d32fb
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-23 23:27:32 +0530

    Program to demonstrate the concept of method in java 10

---
+++ practice-concepts/Method10.java
@@ -0,0 +1,17 @@
+// Program to demonstrate the Methods in java - method overloading
+public class Method10{
+	// we can overload main() - but compiler only picks the default once
+	public static void main(String[] args){	
+		Test t = new Test();
+		String arg1 = null;
+		Integer arg2 = null;
+		t.f(null); // this will produce error - both f() can accept null - no type of null specified
+		t.f(arg1); // type of null value specified i.e. String
+		t.f(arg2); // type of null value specified i.e. Integer
+	}
+}
+class Test{
+	// parameter of object type
+	public void f(String s){ System.out.println("String call"); }
+	public void f(Integer i){ System.out.println("Integer call"); }
+}
