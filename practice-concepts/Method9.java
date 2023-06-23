af546a3fdb6efe99f6cbd76797dbf796dc31a3e0
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-23 23:10:03 +0530

    Program to demonstrate the concept of method in java 9

---
+++ practice-concepts/Method9.java
@@ -0,0 +1,15 @@
+// Program to demonstrate the Methods in java - method overloading
+public class Method6{
+	// we can overload main() - but compiler only picks the default once
+	public static void main(String[] args){		
+	}
+}
+class Test{
+	// methods differ in static keyword cannot be overloaded
+	static void display(String s){}
+	void display(String s){}
+	
+	// methods cannot be overloaded on the basis of return type
+	int display(String s){}
+	char display(String s){}
+}
