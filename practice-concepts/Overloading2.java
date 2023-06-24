e5b572dbef692cc27b4110aae00106a3be048c9a
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-24 22:05:08 +0530

    Program to demonstrate the concept of method overloading 2

---
+++ practice-concepts/Overloading2.java
@@ -0,0 +1,14 @@
+// Program to demonstrate the Methods in java - method overloading
+public class Overloading2{
+	public static void main(String[] args){
+		Test.sum(1,3); 
+		Test.sum(-4.342,232.232);
+		Test.sum('1','a');
+	}
+}
+public class Test{
+	// static methods can be overloaded
+	static void sum(double a,double b){ System.out.println("double type method"); }
+	static void sum(int a,int b){ System.out.println("int type method"); }
+	static void sum(char a,char b){ System.out.println("char type method"); }
+}
