e40842ac4097781eb5900c3f0a90f3568c9117f3
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-24 22:05:08 +0530

    Program to demonstrate the concept of method overloading 3

---
+++ practice-concepts/Overloading3.java
@@ -0,0 +1,18 @@
+// Program to demonstrate the Methods in java - method overloading
+public class Overloading3{
+	// we can overload main() - but compiler only picks the default once
+	public static void main(String[] args){
+		System.out.println("real main()");  
+		// we can explicitly call main()
+		main(2); main("main()", "fake"); main("main()"); 
+	}
+	public static void main(String arg){
+		System.out.println("fake one string main()");  
+	}
+	public static void main(String arg1, String arg2){
+		System.out.println("fake two string main()");  
+	}
+	public static void main(int a){
+		System.out.println("fake one int main()");  
+	}
+}
