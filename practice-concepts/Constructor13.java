0289f5575c2d21704c05da1a9d71bb47fc34e1a2
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-26 19:50:30 +0530

    Program to demonstrate the concept of Constructor 13

---
+++ practice-concepts/Constructor13.java
@@ -0,0 +1,27 @@
+// Program to demonstrate various the concept of constructor - Singleton Class
+public class Constructor13{
+	public static void main(String[] args){
+		Singleton s = Singleton.createObject();
+		s.display();
+		s.i = s.i + 1;
+		s.display();
+		Singleton sref = Singleton.createObject(); // no matter how many reference variable you create they 
+		// will point one constructor
+		sref.display();
+	}
+}
+// Singleton class allows to create only one object
+class Singleton{
+	public static Singleton oneObj = null;
+	int i = 0;
+	void display(){
+		System.out.println("Current i = "+i);
+	}
+	private Singleton(){
+		System.out.println("Private Constructor");
+	}
+	public static Singleton createObject(){
+		if( oneObj == null ) oneObj = new Singleton();
+		return oneObj;
+	}
+}
