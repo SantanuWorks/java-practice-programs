ce86bbe4dbe77302ccb03e4c9d378b54d3627782
Santanu Pradhan <santanuworkspace@gmail.com>
2023-06-28 16:52:21 +0530

    Practice program regarding OOP 2

---
+++ practice-problems/Oops2.java
@@ -0,0 +1,26 @@
+// Create the Person class. Create some objects of this class (by taking information from
+// the user). Inherit the class Person to create two classes Teacher and Student class.
+// Maintain the respective information in the classes and create, display and delete objects
+// of these two classes (Use Runtime Polymorphism)
+public class Oops2{
+	public static void main(String[] args){
+		
+	}
+}
+class Person{
+	int age;
+	String name;
+	void display(){
+		
+	}
+}
+class Teacher extends Person{
+	void display(){
+		
+	}
+}
+class Student extends Person{
+	void display(){
+		
+	}
+}
