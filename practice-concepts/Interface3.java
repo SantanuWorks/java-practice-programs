afa13eef899b068895d9f069c943a5e7d43c86a2
Santanu Pradhan <santanuworkspace@gmail.com>
2023-07-03 22:04:02 +0530

    Program to demonstrate the concept of interface 3

---
+++ practice-concepts/Interface3.java
@@ -0,0 +1,20 @@
+// Program to practice the concept of interface in java.
+public class Interface3{
+	public static void main(String[] args){
+		C obj = new C();
+		obj.f();
+	}
+}
+// Using interfaces implement multiple inheritance
+interface A{ 
+	void f();
+	// we can not have body inside the interfaces
+	// following will produce error
+	void m() {} // dummy method
+}
+interface B{ void f(); }
+class C implements A,B{
+	public void f(){
+		System.out.print("I'm a method");
+	}
+}
