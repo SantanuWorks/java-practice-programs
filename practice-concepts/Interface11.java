a959be40e89d0b7489896366b63ab4b55cca94ef
Santanu Pradhan <santanuworkspace@gmail.com>
2023-07-04 23:06:22 +0530

    Program to demonstrate the concept of interface 11

---
+++ practice-concepts/Interface11.java
@@ -0,0 +1,30 @@
+// Program to practice the concept of interface in java.
+public class Interface11{
+	public static void main(String[] args){
+		impleB ib = new impleB();
+		ib.display();
+		impleD id = new impleD();
+		id.display();
+	}
+}
+// interfaces can be nested inside a class or interface
+interface A{
+	interface B{
+		void display();
+	}
+}
+class impleB implements A.B{
+	public void display(){
+		System.out.println("A.B's display()");
+	}
+}
+class C{
+	interface D{
+		void display();
+	}
+}
+class impleD implements C.D{
+	public void display(){
+		System.out.println("C.D's display()");
+	}
+}
