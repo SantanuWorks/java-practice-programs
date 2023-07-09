bb9f3a6654000483c5728e22d3864f7acbaac667
Santanu Pradhan <santanuworkspace@gmail.com>
2023-07-09 23:43:47 +0530

    Program to demonstrate the concept of wrapper class 1

--- practice-concepts/Wrapper1.java
+++ practice-concepts/Wrapper1.java
@@ -1,11 +1,28 @@
-// Program to demonstrate the concept of Wrapper Class in java
+// Program to demonstrate the concept of Wrapper Class in java - types of wrapper class
 public class Wrapper1{
 	public static void main(String[] args){
-		Integer a = new Integer(2);
-		Integer b = new Integer(2);
-		System.out.println(a==b);
-		Integer c = Integer.valueOf(3);
-		Integer d = Integer.valueOf(3);
-		System.out.print(c==d);
+		Boolean a = true;
+		System.out.println("Boolean Object, a = "+a);
+		
+		Byte b = 1;
+		System.out.println("Byte Object, b = "+b);
+		
+		Character c = 'A';
+		System.out.println("Character Object, c = "+c);
+		
+		Float d = 1.1f;
+		System.out.println("Float Object, d = "+d);
+		
+		Integer e = 2;
+		System.out.println("Integer Object, e = "+e);
+		
+		Long f = 2l;
+		System.out.println("Long Object, f = "+f);
+		
+		Short g = 3;
+		System.out.println("Short Object, g = "+g);
+		
+		Double h = 1.2;
+		System.out.print("Double Object, h = "+h);
 	} 
 }
