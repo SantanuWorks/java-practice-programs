a8263c4a6a93bf85e12dc2e926ed9660af94436a
Santanu Pradhan <santanuworkspace@gmail.com>
2023-07-09 23:43:48 +0530

    Program to demonstrate the concept of wrapper class 2

--- practice-concepts/Wrapper2.java
+++ practice-concepts/Wrapper2.java
@@ -1,6 +1,23 @@
-// Program to demonstrate the concept of Wrapper Class in java
+// Program to demonstrate the concept of Wrapper Class in java - way to create instance of java
 public class Wrapper2{
 	public static void main(String[] args){
+		// Constructor method is deprecated
+		// Integer a = new Integer(1);
 		
+		// Using valueOf() method
+		Integer b = Integer.valueOf(2);
+		System.out.println("Integer Object, b = "+b);
+		
+		// Using autobixing
+		// Internally it uses valueOf()
+		Integer c = 3;
+		System.out.println("Integer Object, c = "+c);
+		 
+		// All the objects created using above two method with same value point to same memory location
+		Integer p = Integer.valueOf(4);
+		Integer q = 4;
+		Integer r = 5;
+		System.out.println("Objects with same value are equal, "+(p==q));
+		System.out.print("Objects with different value are unequal, "+(p==r));
 	} 
 }
