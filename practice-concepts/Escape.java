81e56bd541cb41da5bd3d2059e2ca211e2042378
Santanu Pradhan <santanuworkspace@gmail.com>
2023-07-10 23:41:55 +0530

    Program to demonstrate the escape sequence in java

---
+++ practice-concepts/Escape.java
@@ -0,0 +1,31 @@
+// Program to demonstrate the use of escape sequence in java
+public class Escape{
+	public static void main(String[] args){
+		/* 
+		 * 	\t - Inserts a tab
+		 * 	\b - Inserts a backspace
+		 * 	\n - Inserts a new-line
+		 * 	\r - Inserts a carriage return
+		 * 	\f - Inserts a formfeed
+		 * 	\' - Inserts a single quote
+		 * 	\" - Inserts a double quote
+		 * 	\\ - Inserts a backslash
+		**/ 
+		
+		System.out.println("Escape\tSequence");
+		System.out.println();
+		System.out.println("Escape\bSequence");
+		System.out.println();
+		System.out.println("Escape\nSequence");
+		System.out.println();
+		System.out.println("Escape\rSequence");
+		System.out.println();
+		System.out.println("Escape\fSequence");
+		System.out.println();
+		System.out.println("Escape\'Sequence");
+		System.out.println();
+		System.out.println("Escape\"bSequence");
+		System.out.println();
+		System.out.println("Escape\\Sequence");
+	} 
+}
