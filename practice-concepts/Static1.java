0f3f3451d6bae4dd2e99bd51841cf27359b220c0
Santanu Pradhan <santanuworkspace@gmail.com>
2023-07-17 17:31:03 +0530

    Program to demonstrate the use of static keyword 1 (updated)

--- practice-concepts/Static1.java
+++ practice-concepts/Static1.java
@@ -1,6 +1,29 @@
-// Program to demonstrate the use of static keyword - 
+// Program to demonstrate the use of static keyword - Static Block
+// Static block is used to initialize static variables
 public class Static1{
+	static int a;
+	// executed when main class gets loaded
+	static{
+		a = 0;
+		System.out.println("Main:Static Block Executed 1");
+	}
+	static{
+		System.out.println("Main:Static Block Executed 2");
+	}
 	public static void main(String[] args){
-		
+		A a = new A(); // static block will execute
+		A b = new A();
+		A c = new A();
+	}
+	static{
+		System.out.println("Main:Static Block Executed 3");
+	}
+}
+class A{
+	static int c;
+	// gets executed when first object is created
+	static{
+		c = 0;
+		System.out.println("A:Static Block Executed");
 	}
 }
