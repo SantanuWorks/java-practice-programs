e5eb9f9c0f4d9640f0f0fc250181b122f849372e
Santanu Pradhan <santanuworkspace@gmail.com>
2023-07-17 17:31:04 +0530

    Program to demonstrate the use of static keyword 3 (updated)

--- practice-concepts/Static3.java
+++ practice-concepts/Static3.java
@@ -1,6 +1,21 @@
-// Program to demonstrate the use of static keyword - 
+// Program to demonstrate the use of static keyword - Static Method
+// Belongs to class shared among all objects
 public class Static3{
+	int a;
+	static int b;
+	void display(){ System.out.println("Non-static f()"); }
+	static void show(){ System.out.println("Static f()"); }
+	static void f(){
+		// static methods can not access instance variables and methods
+		// display();
+		// a = 0;
+		// they can only access static members
+		show();
+		b = 1;
+	}
 	public static void main(String[] args){
-		
+		// static method can be called without using object and before creating objects
+		System.out.println(b);
+		f();
 	}
 }
